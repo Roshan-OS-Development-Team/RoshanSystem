e6e6a48d86dde1a80afed26d34068a5ff28b1305
Roshan <mdtahanchy@gmail.com>
2026-02-26 21:10:53 +0000

    Went from Python to C#

---
+++ New Roshan System/Roshan System/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Roshan_System
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Notepad(object sender, RoutedEventArgs e)
+        {
+            Notepad notepad = new Notepad();
+            notepad.Show();
+        }
+
+        private void Open_Calculator(object sender, RoutedEventArgs e)
+        {
+            Calculator calculator = new Calculator();
+            calculator.Show();
+        }
+    }
+}
---
+++ New Roshan System/Roshan System/Notepad.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Roshan_System
+{
+    /// <summary>
+    /// Interaction logic for Notepad.xaml
+    /// </summary>
+    public partial class Notepad : Window
+    {
+        public Notepad()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_File(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.Title = "Save Notepad Content";
+            saveFileDialog.ShowDialog();
+            string text = NotepadTextBox.Text;
+            File.WriteAllText(saveFileDialog.FileName, text);
+            MessageBox.Show("File saved to:", saveFileDialog.FileName);
+        }
+
+        private void Load_File(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text file (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.Title = "Open Notepad Content";
+            openFileDialog.ShowDialog();
+            string text = File.ReadAllText(openFileDialog.FileName);
+            NotepadTextBox.Text = text;
+        }
+    }
+}
