4ba351378baac393101f87a0557957e5e132b10d
Roshan <mdtahanchy@gmail.com>
2026-04-22 18:31:32 +0100

    Added requirments and start menu and a new background :)

---
+++ Roshan System - Python Version/gui/startmenu.py
@@ -0,0 +1,6 @@
+import customtkinter as ctk
+
+
+class StartMenu(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master, width=300, height=400)
--- Roshan System - Python Version/main.py
+++ Roshan System - Python Version/main.py
@@ -2,6 +2,7 @@ from PIL import Image
 import customtkinter as ctk
 from gui.taskbar import Taskbar
 from gui.notepad import Notepad
+from gui.startmenu import StartMenu
 
 
 class App(ctk.CTk):
@@ -10,7 +11,7 @@ class App(ctk.CTk):
         self.title("Roshan System")
         self.geometry("1200x800")
         self.attributes(fullscreen=True)
-        self.backgroundimg = Image.open("textures/background5.png")
+        self.backgroundimg = Image.open("textures/background9.png")
         self.backgroundctk = ctk.CTkImage(self.backgroundimg, size=(1200, 800))
         self.background = ctk.CTkLabel(self, text="", image=self.backgroundctk)
         self.background.pack(fill="both", side="top")
@@ -21,8 +22,39 @@ class App(ctk.CTk):
             corner_radius=50,
             fg_color="gray7",
         )
+
+        # Start Menu Stuff
+        startbtnimg = Image.open("textures/startmenu.png")
+        startbtnimgctk = ctk.CTkImage(startbtnimg, size=(70, 70))
+        self.startbtn = ctk.CTkButton(
+            self.taskbar,
+            image=startbtnimgctk,
+            text="",
+            bg_color="transparent",
+            fg_color="transparent",
+            width=70,
+            height=70,
+            hover_color="#353434",
+            command=self.toggle_start_menu,
+        )
+        self.startbtn.pack(side="left")
+        self.startmenu = StartMenu(self.background)
+        self.startmenuopened = False
+
+        # Notepad Stuff
+        notepadimg = Image.open("textures/notepad.png")
+        notepadimgctk = ctk.CTkImage(notepadimg, size=(40, 51))
         self.notepadbtn = ctk.CTkButton(
-            self.taskbar, text="Open Notepad", command=self.open_notepad
+            self.taskbar,
+            border_width=0,
+            image=notepadimgctk,
+            width=70,
+            height=70,
+            hover_color="#353434",
+            text="",
+            fg_color="transparent",
+            bg_color="transparent",
+            command=self.open_notepad,
         )
         self.notepadbtn.pack(side="left")
         self.notepad = Notepad(self)
@@ -37,6 +69,14 @@ class App(ctk.CTk):
             self.taskbar.configure(width=self.winfo_width(), height=70)
             self.taskbar.pack_propagate(False)
             self.taskbar.place(x=0, y=self.winfo_height() - 70)
+
+    def toggle_start_menu(self):
+        if not self.startmenuopened:
+            self.startmenu.place(x=10, y=self.winfo_height() - 480)
+            self.startmenuopened = True
+        else:
+            self.startmenu.place_forget()
+            self.startmenuopened = False
 
 
 if __name__ == "__main__":
