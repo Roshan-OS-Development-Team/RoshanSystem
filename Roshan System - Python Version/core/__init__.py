d6872bdc30deeb8136595e89b7d7d9de6aced2b6
Roshan <mdtahanchy@gmail.com>
2026-08-20 16:42:57 +0100

    Finished the switch widget ig

--- Roshan System - Python Version/core/__init__.py
+++ Roshan System - Python Version/core/__init__.py
@@ -3,12 +3,18 @@ from .filedialog import (  # Imports everything from file dialog
     SaveFileDialog,
 )
 from .htmlfilesticher import stichHTMLFile  # Imports the HTML File Stitcher
+from .qtSwitch import (  # Imports everything needed from qtSwitch
+    HorizontalSwitch,
+    VerticalSwitch,
+)
 from .styling import get_qss_styles  # Imports styling
 from .Window import WebWindow, Window  # Imports everything from Window
 
 __all__ = (
+    "HorizontalSwitch",
     "OpenFileDialog",
     "SaveFileDialog",
+    "VerticalSwitch",
     "WebWindow",
     "Window",
     "get_qss_styles",
--- Roshan System - Python Version/core/qtSwitch.py
+++ Roshan System - Python Version/core/qtSwitch.py
@@ -2,6 +2,7 @@ from collections.abc import Callable
 from typing import Any
 
 from PySide6.QtWidgets import (  # Imports everything needed for the Switch
+    QApplication,  # QApplication for the testing
     QHBoxLayout,  # QHBoxLayout for Horizontal Switch
     QPushButton,  # QPushButton for the buttons
     QVBoxLayout,  # QVBoxLayout for the Vertical Switch
@@ -19,4 +20,48 @@ class QtSwitch(QWidget):
     def __init__(self, master, offLabel: str, onLabel: str, QLayout, onChangeCallback: Callable[[bool], Any]) -> None:
         super().__init__(master)
         self._layout: QVBoxLayout | QHBoxLayout = QLayout(self)
+        self.onChangeCallback = onChangeCallback
         self.switchedOn: bool = False
+
+        self.off_btn = QPushButton(self)
+        self.off_btn.setStyleSheet(style["selected_btn"])
+        self.off_btn.setText(offLabel)
+        self.off_btn.clicked.connect(self.handleSwitch)
+        self._layout.addWidget(self.off_btn)
+
+        self.on_btn = QPushButton(self)
+        self.on_btn.setStyleSheet(style["unselected_btn"])
+        self.on_btn.setText(onLabel)
+        self.on_btn.clicked.connect(self.handleSwitch)
+        self._layout.addWidget(self.on_btn)
+
+        self.selected: bool = False
+
+    def handleSwitch(self):
+        if self.selected:
+            self.off_btn.setStyleSheet(style["selected_btn"])
+            self.on_btn.setStyleSheet(style["unselected_btn"])
+            self.onChangeCallback(False)
+        else:
+            self.off_btn.setStyleSheet(style["unselected_btn"])
+            self.on_btn.setStyleSheet(style["selected_btn"])
+            self.onChangeCallback(True)
+        self.selected = not self.selected
+
+class VerticalSwitch(QtSwitch):
+    def __init__(self, master = None, offLabel: str = "Off", onLabel: str = "On", onChangeCallback: Callable[[bool], Any] = print) -> None:
+        super().__init__(master, offLabel, onLabel, QVBoxLayout, onChangeCallback)
+
+class HorizontalSwitch(QtSwitch):
+    def __init__(self, master = None, offLabel: str = "Off", onLabel: str = "On", onChangeCallback: Callable[[bool], Any] = print) -> None:
+        super().__init__(master, offLabel, onLabel, QHBoxLayout, onChangeCallback)
+
+if __name__ == "__main__":
+    app = QApplication(["--style=fusion"])
+    test1 = VerticalSwitch()
+    test1.resize(100, 50)
+    test2 = HorizontalSwitch()
+    test2.resize(100, 50)
+    test1.show()
+    test2.show()
+    app.exec()
