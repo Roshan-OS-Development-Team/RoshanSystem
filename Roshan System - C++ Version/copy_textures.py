4486c5c7c2ed3bb85a9f90d781890583a568fb1e
Roshan <mdtahanchy@gmail.com>
2026-09-05 12:19:48 +0100

    Added copy_textures.py to copy textures from the python version

---
+++ Roshan System - C++ Version/copy_textures.py
@@ -0,0 +1,8 @@
+﻿import os
+import shutil
+import colorama
+
+os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+shutil.copytree("Roshan System - Python Version/textures", "Roshan System - C++ Version/textures", dirs_exist_ok=True)
+print(colorama.Fore.BLUE + "Copied textures from the python version to the C++ version" + colorama.Fore.RESET)
